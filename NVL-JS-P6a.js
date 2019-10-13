05cd2c0cb90b491c58559dc3809f1bcc3f76e10a
Tachikoma82 <losdomingosdejuano@gmail.com>
2019-10-13 20:44:32 +0200

    javascript - variables

---
+++ NVL-JS-P6a.js
@@ -0,0 +1,5 @@
+var miNombreCompleto = 'Juan Manuel Martínez Vela';
+var miEdad = 37;
+var diasSemana = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'];
+var gravedad = 9.8;
+var numImpares = [1,3,5];
